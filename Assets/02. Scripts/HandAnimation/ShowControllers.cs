ba9be33ffbcc027c4c9825a36528bc12cab92c87
Mellow1213 <dpcks7282@gmail.com>
2023-04-15 18:29:51 +0900

    Add ShowController.cs

---
+++ Assets/02. Scripts/HandAnimation/ShowControllers.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ShowControllers : MonoBehaviour
+{
+    public bool showController = false;
+    
+    void Update()
+    {
+        foreach (var hand in Player.instance.hands)
+        {
+            if (showController)
+            {
+                hand.ShowController();
+            }
+            else
+            {
+                hand.HideController();
+            }
+        }
+        
+    }
+}
