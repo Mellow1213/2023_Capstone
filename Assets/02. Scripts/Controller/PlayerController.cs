2ff2794207ccf90ac661f2c9b8b0306ea69f5e9b
Mellow1213 <dpcks7282@gmail.com>
2023-04-20 12:55:41 +0900

    View-related movement

--- Assets/02. Scripts/Controller/PlayerController.cs
+++ Assets/02. Scripts/Controller/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
+
 public class PlayerController : MonoBehaviour
 {
     public SteamVR_Action_Vector2 _input;
@@ -11,6 +13,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += speed * Time.deltaTime * new Vector3(_input.axis.x, 0, _input.axis.y);
+        Vector3 direction =
+            Player.instance.hmdTransform.TransformDirection(new Vector3(_input.axis.x, 0, _input.axis.y));
+        transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) ;
     }
 }
