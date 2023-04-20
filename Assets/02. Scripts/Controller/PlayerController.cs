babcc65351d19ad13384f82fa2384b3ae5424caa
Mellow1213 <dpcks7282@gmail.com>
2023-04-20 14:43:30 +0900

    Haptic Bug Fix

--- Assets/02. Scripts/Controller/PlayerController.cs
+++ Assets/02. Scripts/Controller/PlayerController.cs
@@ -21,7 +21,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
-        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up));
+        if (input.axis.magnitude > 0.1f)
+        {
+            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+            characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up)- new Vector3(0, 9.81f, 0) * Time.deltaTime);
+        }
     }
 }
