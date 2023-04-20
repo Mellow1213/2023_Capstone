f443a5c641b825eea2a616c93fb6645366329c55
Mellow1213 <dpcks7282@gmail.com>
2023-04-20 15:07:22 +0900

    gravity bug fix

--- Assets/02. Scripts/Controller/PlayerController.cs
+++ Assets/02. Scripts/Controller/PlayerController.cs
@@ -21,10 +21,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (input.axis.magnitude > 0.1f)
-        {
             Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
             characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up)- new Vector3(0, 9.81f, 0) * Time.deltaTime);
-        }
+        
     }
 }
