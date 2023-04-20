13f04c5a2dac3b1f1edd5553377ae38ff80bf0a6
Mellow1213 <dpcks7282@gmail.com>
2023-04-20 13:49:17 +0900

    VR Key-Binding Reset

--- Assets/02. Scripts/Controller/PlayerController.cs
+++ Assets/02. Scripts/Controller/PlayerController.cs
@@ -6,15 +6,14 @@ using Valve.VR.InteractionSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public SteamVR_Action_Vector2 _input;
+    public SteamVR_Action_Vector2 input;
 
     public float speed = 1;
     
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction =
-            Player.instance.hmdTransform.TransformDirection(new Vector3(_input.axis.x, 0, _input.axis.y));
-        transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) ;
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+        transform.position += speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up);
     }
 }
