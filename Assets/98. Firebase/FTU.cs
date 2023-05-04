5e84094d79502aa25c96987dd7ed8c002b77b640
BADA LEE <dlqkek8@naver.com>
2023-05-04 16:08:21 +0900

    Firebase to Unity 데이터 로그 찍음

---
+++ Assets/98. Firebase/FTU.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+
+
+public class FTU : MonoBehaviour // Firebase to Unity
+{
+    private DatabaseReference database;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 데이터베이스의 루트 참조 위치 가져옴
+        database = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))    // 왼쪽 마우스 클릭 시
+        {
+            database.ValueChanged += HandleValueChanged;    // 데이터가 변경될 때마다 읽음
+        }
+    }
+
+    void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        if(args.DatabaseError != null) 
+        {
+            Debug.Log(args.DatabaseError.Message); // 에러 메세지
+            return;
+        }
+        Debug.Log(args.Snapshot.Child("bpm").Value); // bpm에 있는 데이터 로그 찍음
+    }
+}
