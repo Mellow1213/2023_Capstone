eede7565dc7f55eb5a68b80ba2cb6dd40d6f3438
tjdbssy137 <65931605+tjdbssy137@users.noreply.github.com>
2023-05-08 21:19:42 +0900

    script
    script 추가 및 수정

---
+++ Assets/02. Scripts/UnderAss_R/LightDetail.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDetail : MonoBehaviour
+{
+    //public GameObject illusion;
+    private Light theLight;
+    private float targetIntensity;
+    private float currentIntensity;
+
+    void Start()
+    {
+        theLight = GetComponent<Light>();
+        currentIntensity = theLight.intensity;
+        targetIntensity = Random.Range(0.0f, 4.0f);
+    }
+
+    void Update()
+    {
+        if (Mathf.Abs(targetIntensity - currentIntensity) >= 0.01)
+        {
+            if (targetIntensity - currentIntensity >= 0)
+                currentIntensity += Time.deltaTime * 10f;
+            else
+            {
+                currentIntensity -= Time.deltaTime * 10f;
+            }
+            theLight.intensity = currentIntensity;
+            theLight.range = currentIntensity + 7;
+        }
+        else
+        {
+            targetIntensity = Random.Range(0.0f, 4.0f);
+        }
+    }
+}
