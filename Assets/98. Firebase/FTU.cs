746c3c5f0604e5c38711592e178b9a704e7c8a28
Mellow1213 <dpcks7282@gmail.com>
2023-05-09 14:44:28 +0900

    TMPro 추가

--- Assets/98. Firebase/FTU.cs
+++ Assets/98. Firebase/FTU.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
 using Firebase.Database;
+using TMPro;
 
 
 public class FTU : MonoBehaviour // Firebase to Unity
 {
     private DatabaseReference database;
+    public TextMeshProUGUI BPM_TextMeshPro;
+    private string bpm;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,22 @@ public class FTU : MonoBehaviour // Firebase to Unity
         database = FirebaseDatabase.DefaultInstance.RootReference;
         database.ValueChanged += HandleValueChanged;    // 데이터가 변경될 때마다 읽음
     }
+
+    private string bpmValue;
+    private string[] bpmValues;
+    private float timer = 0;
     
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 1)
+        {
+            timer = 0;
+            Debug.Log("출력");
+            BPM_TextMeshPro.text = bpmValues[0];
+        }
+    }
+
     void HandleValueChanged(object sender, ValueChangedEventArgs args)
     {
         if(args.DatabaseError != null) 
@@ -23,6 +42,7 @@ public class FTU : MonoBehaviour // Firebase to Unity
             Debug.Log(args.DatabaseError.Message); // 에러 메세지
             return;
         }
-        Debug.Log(args.Snapshot.Child("bpm").Value); // bpm에 있는 데이터 로그 찍음
+        bpmValue = args.Snapshot.Child("bpm").Value.ToString(); // bpm 값 가져오기
+        bpmValues = bpmValue.Split(' '); // 스페이스바를 기준으로 문자열 나누기
     }
 }
