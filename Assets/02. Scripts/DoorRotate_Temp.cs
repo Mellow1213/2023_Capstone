45dabc0f9617b2ddea010e409841d374c92b00a7
Mellow1213 <dpcks7282@gmail.com>
2023-05-20 13:23:44 +0900

    Door Rotate Scripts

---
+++ Assets/02. Scripts/DoorRotate_Temp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+public class DoorRotate_Temp : MonoBehaviour
+{
+    public GameObject target;
+
+    private Vector3 T;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = target.transform.position;
+        T = new Vector3(position.x, transform.position.y, position.z);
+        
+        transform.LookAt(T);
+    }
+}
