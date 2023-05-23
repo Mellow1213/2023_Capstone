bf9144bef13678597cc17386474950df1487a1c2
tjdbssy137 <65931605+tjdbssy137@users.noreply.github.com>
2023-05-24 02:52:06 +0900

    완성

--- Assets/02. Scripts/UnderAss_R/VolumeDown.cs
+++ Assets/02. Scripts/UnderAss_R/VolumeDown.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class VolumeDown : MonoBehaviour
 {
     public AudioClip event1;
     public AudioClip[] event2;
     private AudioSource m_Source;
+
+    private bool sEvent1 = false;
+    private bool sEvent3 = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +22,18 @@ public class VolumeDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.O))//¾î¶°ÇÑ ½É¹Ú¼ö¿¡ µµ´ŞÇÑ´Ù¸é
+        if (FTU.Instance.BPMEvent >= 1 && !sEvent1)//ì–´ë– í•œ ì‹¬ë°•ìˆ˜ì— ë„ë‹¬í•œë‹¤ë©´
+        {
             StartCoroutine(event1Sound());
-        if (Input.GetKeyDown(KeyCode.P))//¾î¶°ÇÑ ½É¹Ú¼ö¿¡ µµ´ŞÇÑ´Ù¸é
+            sEvent1 = true;
+            StartCoroutine(changeTFforEvent1());
+        }
+        else if (FTU.Instance.BPMEvent >= 3 && !sEvent3)//ì–´ë– í•œ ì‹¬ë°•ìˆ˜ì— ë„ë‹¬í•œë‹¤ë©´
+        {
             StartCoroutine(event2Sound());
-
+            sEvent3 = true;
+            StartCoroutine(changeTFforEvent3());
+        }
     }
 
     IEnumerator event1Sound()
@@ -48,5 +60,15 @@ public class VolumeDown : MonoBehaviour
         yield return new WaitForSeconds(1);
         m_Source.clip = event2[i];
         m_Source.PlayOneShot(m_Source.clip);
+    }
+    IEnumerator changeTFforEvent1()
+    {
+        yield return new WaitForSeconds(120);
+        sEvent1 = false;
+    }
+    IEnumerator changeTFforEvent3()
+    {
+        yield return new WaitForSeconds(120);
+        sEvent3 = false;
     }
 }
